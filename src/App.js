87182da17ff0bd8da36c31ce0d1f6764dde98aa4
Marcin Barszcz <41489589+marcinnnnb@users.noreply.github.com>
2023-01-02 16:49:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navigation from 'components/Navigation/Navigation';
+import Welcome from './components/Welcome/Welcome';
+import { GlobalStyle } from './assets/styles/GlobalStyles';
+import InfiniteScroll from './episodes/InfiniteLoading/InfiniteScroll';
+import AccordionFaq from './episodes/AccordionFaq/AccordionFaq';
+import Sidebar from './episodes/Sidebar/Sidebar';
+import Combobox from './episodes/Combobox/Combobox';
+import FancyButtons from './episodes/FancyButtons/FancyButtons';
+import CoolTransitions from './episodes/CoolTransitions/CoolTransitions';
+import AnimatedSvg from './episodes/AnimatedSvg/AnimatedSvg';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/animated-svg">
+            <AnimatedSvg />
+          </Route>
+          <Route path="/cool-transitions">
+            <CoolTransitions />
+          </Route>
+          <Route path="/fancy-buttons">
+            <FancyButtons />
+          </Route>
+          <Route path="/combobox">
+            <Combobox />
+          </Route>
+          <Route path="/infinite-scroll">
+            <InfiniteScroll />
+          </Route>
+          <Route path="/accordion-faq">
+            <AccordionFaq />
+          </Route>
+          <Route path="/sidebar">
+            <Sidebar />
+          </Route>
+          <Route path="/">
+            <Welcome />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ src/assets/styles/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    max-width: 100%;
+    width: 100%;
+    overflow-x: hidden;
+  }
+
+  *, *::after, *::before {
+    box-sizing: inherit;
+    font-family: 'IBM Plex Mono', monospace;
+    color: black;
+  }
+`;
