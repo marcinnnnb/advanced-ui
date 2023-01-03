863ea1f14961189820340e47783a567c51f68557
Marcin Barszcz <marcin.barszcz@op.pl>
2023-01-03 17:21:31 +0100

    feature: cool transition

--- src/episodes/CoolTransitions/Demo1.js
+++ src/episodes/CoolTransitions/Demo1.js
@@ -33,11 +33,11 @@ const SlideLeftStart = keyframes`
 `;
 
 const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  height: 100vh;
 
   &::before {
     content: '';
